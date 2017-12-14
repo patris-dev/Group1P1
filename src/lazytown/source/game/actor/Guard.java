5f7a475e99393859cd540933b1990fb610f789f9
Mark Sahlgreen <mark@nozzy.org>
2017-12-14 11:11:35 +0100

    Initial collision for the guards, to collide with objects in our level - does not work

--- src/lazytown/source/game/actor/Guard.java
+++ src/lazytown/source/game/actor/Guard.java
@@ -5,6 +5,7 @@ import javafx.scene.shape.SVGPath;
 import javafx.scene.shape.Shape;
 import lazytown.source.Main;
 import lazytown.source.game.Game;
+import lazytown.source.game.UI;
 import lazytown.source.game.level.Tile;
 
 /**
@@ -38,6 +39,7 @@ public class Guard extends MovedActor {
      */
     @Override
     public void update() {
+        checkCollision();
         setXYLocation();
         setImageState();
         moveCharacter();
@@ -139,6 +141,50 @@ public class Guard extends MovedActor {
                 } else {framecounter += 1;}
             }
         }
+    }
+
+    private void checkCollision() {
+        // When we check for collision we cycle through the list of all of our actors
+        for (int i = 0; i < Game.director.getCurrentActors().size(); i++) {
+            // And as we do that we set them up in a temporary object
+            Actor object = Game.director.getCurrentActors().get(i);
+            // This object is then being parsed and tested in our collide method
+            if (collide(object)) {
+
+                if (object instanceof Tile || object instanceof InteractiveActor) {
+                    if (!facingUp) {
+                        iY -= velY;
+                    }
+                    else if (facingUp) {
+                        iY += velY;
+                    }
+                    if (!facingLeft) {
+                        iX -= velX;
+                    }
+                    else if (facingLeft) {
+                        iX += velX;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * We check for collision in a two stage process, this is to save resources, as checking for collision can be
+     * quite costly. The way we do this is by first using an if statement to check if two imageView nodes intersect
+     * with each other, if they do we create a new Shape object from the two intersecting ImageView nodes, the width
+     * of which we measure. If this width is not negative 1, we return true, else we return false.
+     * @param object object which is checked for collision.
+     */
+    private boolean collide(Actor object){
+        if (object.getSpriteFrame().getBoundsInParent().intersects(
+                iX+levelWidth/2-20, iY+levelHeight/2-37.5, 40, 75)) {
+            Shape intersection = SVGPath.intersect(Game.playerOne.getSpriteBoundary(), object.getSpriteBoundary());
+            if (intersection.getBoundsInLocal().getWidth() != -1) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private void moveCharacter() {
